fd46451bbe9560c989d7d68e6b37ff3f3f7b6ff2
Andrew Maier <andrew.maier@gsa.gov>
2017-01-03 14:54:24 -0500

    Wire up functionality on form

--- javascripts/application.js
+++ javascripts/application.js
@@ -13,21 +13,4 @@ $(document).ready(function() {
     $(this).parents('.field').removeClass('is-focused')
   });
 
-  $('.layout-claims-new')
-    .on("change", 'select[name="information.reason"]', function(){
-      if ($(this).val() == "Manage a recently diagnosed medical condition.") {
-        $('.dependent').hide();
-      } else {
-        $('.dependent').show();
-      }
-    })
-    .on("change", 'input[name="payment.method"]', function(){
-      var val = $('input[name="payment.method"]:checked').val();
-      if (val == "direct deposit") {
-        $('.direct_deposit').show();
-      } else {
-        $('.direct_deposit').hide();
-      }
-    });
-
 });
